af514a89a40d3842edcfacb13cdcbde8563efd45
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-02-07 10:25:16 -0300

    Añadiendo test y esqueleto html

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,10 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "abcdefghijklmnopqrstuvwxyz" con offset 33');
+    it('debería retornar "3456789012" para "1234567890" con offset 2');
+  it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+  it('debería retornar "!#$%&/()=" para "!#$%&/()=" con offset 2');
   });
 
   describe('cipher.decode', () => {
@@ -18,8 +21,10 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
-
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "hijklmnopqrstuvwxyzabcdefg"con offset 33');
+    it('debería retornar "1234567890" para "3456789012" con offset 2');
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "!#$%&/()=" para "!#$%&/()=" con offset 2');
   });
 
 });
