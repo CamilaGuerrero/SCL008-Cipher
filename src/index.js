d3c69c13e94554fb7a66654175c57718af8ffef1
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-02-12 21:03:50 -0300

    añadiendo mi muy pequeño dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,159 @@
-/* Acá va tu código */
+window.onload = () => {
+    document.getElementById("page0").style.display="block";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none";
+}
+
+//botón comenzar página 0, pasa a la página 1 
+document.getElementById("go").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display ="block";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none"
+});
+
+
+//Esconde pagina 1 y muestra la 2
+document.getElementById("start").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="block";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none"
+});
+
+//función para cipher
+document.getElementById('cipher').addEventListener('click', (evento) => {
+    evento.preventDefault();
+    let inputTextCipher = document.getElementById('enterText').value;
+    let offsetCipher =(document.getElementById('offset').value);
+    document.getElementById("newtext").innerHTML = window.cipher.encode(inputTextCipher, offsetCipher);
+ })
+
+// función para descifrar
+ document.getElementById('decipher').addEventListener('click', (evento) => {
+    evento.preventDefault();
+    let inputTextDecipher = document.getElementById('enterText').value;
+    let offsetDecipher =(document.getElementById('offset').value);
+    document.getElementById("newtext").innerHTML = window.cipher.decode(inputTextDecipher, offsetDecipher);
+ })
+
+ //Copia el texto que hay en la caja de texto 
+document.getElementById("copyBtn").addEventListener("click",() => {
+    let copied = document.getElementById("newText");
+    copied.select();
+    document.execCommand('copy');
+});
+
+
+//Limpia las cajas de texto
+document.getElementById("cleanBox").addEventListener("click",() => {
+    document.getElementById("enterText").value = "";
+    document.getElementById("newText").value = "";
+});
+
+//botón ir a mi inicio pagina 2
+document.getElementById("origin").addEventListener("click",()=>{
+document.getElementById("page0").style.display="block";
+document.getElementById("page1").style.display ="none";
+document.getElementById("page2").style.display="none";
+document.getElementById("page3").style.display ="none";
+document.getElementById("page4").style.display="none";
+});
+
+//botón ir a mi inicio  pagina 4
+document.getElementById("init").addEventListener("click",(evento)=>{
+    event.preventDefault();
+    document.getElementById("page0").style.display="block";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none";
+    });
+    
+//muestra pagina 3 desde pagina 1
+document.getElementById("instrucciones de uso").addEventListener("click",()=>{
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display ="block";
+    document.getElementById("page4").style.display="none";
+    });
+
+//botón ir a mi inicio pagina 3
+document.getElementById("inicio").addEventListener("click",(evento)=>{
+    event.preventDefault();
+    document.getElementById("page0").style.display="block";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none";
+    });    
+
+//botón ir a mi cifrado pagina 3
+document.getElementById("cifrado").addEventListener("click",()=>{
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="block";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none";
+    });
+
+
+
+//botón ir a mi cifrado pagina 4
+document.getElementById("pageCipher").addEventListener("click",()=>{
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display ="none";
+    document.getElementById("page2").style.display="block";
+    document.getElementById("page3").style.display ="none";
+    document.getElementById("page4").style.display="none";
+    });
+
+//botón ir a página 4 desde página 1    
+document.getElementById("help").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="none"
+    document.getElementById("page4").style.display="block"
+});
+
+//botón ir a página 3 desde página 2 
+document.getElementById("explain").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="block"
+    document.getElementById("page4").style.display="none"
+});
+
+//botón ir a página 4 desde página 2
+document.getElementById("helpPages").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="none"
+    document.getElementById("page4").style.display="block"
+});
+
+//botón ir a página 4 desde página 3
+document.getElementById("helping").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="none"
+    document.getElementById("page4").style.display="block"
+});
+
+//botón ir a página 3 desde página 4
+document.getElementById("explaining").addEventListener("click",() => {
+    document.getElementById("page0").style.display="none";
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="block"
+    document.getElementById("page4").style.display="none"
+});
