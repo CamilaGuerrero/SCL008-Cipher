97175c4a6087e505b6034281bbdb55297a842901
CamilaGuerrero <cguerreroverdejo@gmail.com>
2019-02-12 21:29:24 -0300

    añadiendo funciones completadas, cifran y descifran numeros, minusculas, mayusculas, numeros, todo tipo de caracter, tanto con offset negativo como positivo

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,61 @@
 window.cipher = {
-  //se declara la función con los parámetros
   encode: (inputTextCipher, offsetCipher) => {
-    //se establece una variable vacía que recibirá el texto a cifrar
     let encodeText = "";
-    //con un for se recorre el texto a cifrar
     for(let i = 0; i < inputTextCipher.length; i++) {
-    //sacar el código ASCII en cada recorrido
       let asciiTextCipher = inputTextCipher[i].charCodeAt();
       if( asciiTextCipher >= 65 && asciiTextCipher <= 90 ) {
-        let cipherUpper = (asciiTextCipher - 65 + parseInt(offsetCipher)) % 26 + 65;
-        encodeText += String.fromCharCode(cipherUpper);
+        let cipherUpper = (asciiTextCipher - 65 + parseInt(offsetCipher));
+        while (cipherUpper < 0){
+          cipherUpper = (cipherUpper + 26)
+        }
+        let cipherUpper1 = (cipherUpper) %26 + 65;
+        encodeText += String.fromCharCode(cipherUpper1);
       }
       else if ( asciiTextCipher >=97 && asciiTextCipher <= 122 ) {
-           let cipherLow = (asciiTextCipher - 97 + parseInt(offsetCipher)) % 26 + 97;
-          encodeText += String.fromCharCode(cipherLow);
+           let cipherLow = (asciiTextCipher - 97 + parseInt(offsetCipher));
+           while(cipherLow < 0){
+             cipherLow = (cipherLow + 26)
+           }
+           let cipherLow1 = (cipherLow) % 26 + 97; 
+           encodeText += String.fromCharCode(cipherLow1);
       }
       else if (asciiTextCipher >=48 && asciiTextCipher <= 57 ){
-          let cipherNumber = (asciiTextCipher - 48 + parseInt(offsetCipher)) % 10 + 48;
-          encodeText += String.fromCharCode(cipherNumber);
+          let cipherNumber = (asciiTextCipher - 48 + parseInt(offsetCipher));
+          while (cipherNumber < 0){
+            cipherNumber = (cipherNumber + 10)
+          }
+          let cipherNumber1 = (cipherNumber) % 10 + 48;
+          encodeText += String.fromCharCode(cipherNumber1);
       } else {
           encodeText += String.fromCharCode(asciiTextCipher);
       }    }
         return encodeText;
  },
- decode: (inputTextDecipher, offsetDecipher) => {
-  //se establece una variable vacía que recibirá el texto a cifrar
-  let decodeText = "";
-  //con un for se recorre el texto a cifrar
+decode: (inputTextDecipher, offsetDecipher) => {
+    let decodeText = "";
   for(let i = 0; i < inputTextDecipher.length; i++) {
-  //sacar el código ASCII en cada recorrido
     let asciiTextDecipher = inputTextDecipher[i].charCodeAt();
     if( asciiTextDecipher >= 65 && asciiTextDecipher <= 90 ) {
-      let decipherUpper = (asciiTextDecipher - 65 - parseInt(offsetDecipher)) % 26 + 65;
-      decodeText += String.fromCharCode(decipherUpper);
-    } else if ( asciiTextDecipher >=97 && asciiTextDecipher <= 122 ) {
-         let decipherLow = (asciiTextDecipher - 97 - parseInt(offsetDecipher)) % 26 + 97;
-        decodeText += String.fromCharCode(decipherLow);
+      let decipherUpper = (asciiTextDecipher - 65 - parseInt(offsetDecipher));
+      while (decipherUpper < 0){
+        decipherUpper = (decipherUpper + 26);
+      }
+      let decipherUpper1 = (decipherUpper) % 26 + 65;
+      decodeText += String.fromCharCode(decipherUpper1);
+} else if ( asciiTextDecipher >=97 && asciiTextDecipher <= 122 ) {
+         let decipherLow = (asciiTextDecipher - 97 - parseInt(offsetDecipher));
+         while (decipherLow < 0){
+           decipherLow = (decipherLow + 26)
+         }
+         let decipherLow1 = (decipherLow) % 26 + 97;
+        decodeText += String.fromCharCode(decipherLow1);
     } else if (asciiTextDecipher >=48 && asciiTextDecipher <= 57 ){
-        let decipherNumber = (asciiTextDecipher - 48 - parseInt(offsetDecipher)) % 10 + 48;
-        decodeText += String.fromCharCode(decipherNumber);
+        let decipherNumber = (asciiTextDecipher - 48 - parseInt(offsetDecipher));
+        while (decipherNumber < 0){
+          decipherNumber = (decipherNumber + 10)
+        }
+        let decipherNumber1 = (decipherNumber) % 10 + 48;
+        decodeText += String.fromCharCode(decipherNumber1);
     } else {
         decodeText += String.fromCharCode(asciiTextDecipher);
     }    
